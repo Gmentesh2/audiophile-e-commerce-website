ab739b310906b22cd7418d7d25433c00bef1f23f
g.menteshashvili.sg18 <g.menteshashvili.sg18@gipa.ge>
2025-03-17 18:51:50 -0700

    Add responsive HomePage &  Products

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Link } from "react-router-dom";
 import styles from "./footer.module.css";
 import facebookIcon from "../../assets/shared/desktop/icon-facebook.svg";
@@ -10,34 +9,37 @@ const Footer = () => {
   return (
     <footer className={`${styles.footer}`}>
       <div className={`container ${styles.footerContainer}`}>
-        <div className={styles.left}>
-          <img src={logo} alt="" />
+        <div className={styles.footerSection}>
+          <div className={styles.header}>
+            <img src={logo} alt="" />
+            <nav className={styles.nav}>
+              <Link to={"/"}>Home</Link>
+              <Link to={"/products/headphones"}>Headphones</Link>
+              <Link to={"/products/speakers"}>Speakers</Link>
+              <Link to={"/products/earphones"}>Earphones</Link>
+            </nav>
+          </div>
           <p className={styles.p}>
             Audiophile is an all in one stop to fulfill your audio needs. We're
             a small team of music lovers and sound specialists who are devoted
             to helping you get the most out of personal audio. Come and visit
             our demo facility - we’re open 7 days a week.
           </p>
-          <p className={styles.p2}>Copyright 2021. All Rights Reserved</p>
-        </div>
-        <div className={styles.right}>
-          <nav>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/products/headphones"}>Headphones</Link>
-            <Link to={"/products/speakers"}>Speakers</Link>
-            <Link to={"/products/earphones"}>Earphones</Link>
-          </nav>
-          <ul>
-            <li>
-              <img src={facebookIcon} alt="" />
-            </li>
-            <li>
-              <img src={twitterIcon} alt="" />
-            </li>
-            <li>
-              <img src={instagramIcon} alt="" />
-            </li>
-          </ul>
+
+          <div className={styles["down-section"]}>
+            <p className={styles.p2}>Copyright 2021. All Rights Reserved</p>
+            <ul>
+              <li>
+                <img src={facebookIcon} alt="" />
+              </li>
+              <li>
+                <img src={twitterIcon} alt="" />
+              </li>
+              <li>
+                <img src={instagramIcon} alt="" />
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Link } from "react-router-dom";
 import styles from "./navbar.module.css";
 import { IoCartOutline } from "react-icons/io5";
@@ -6,32 +5,39 @@ import { useState } from "react";
 import Modal from "react-modal";
 import CartContent from "../cart-content/CartContent";
 import logo from "../../assets/audiophile.svg";
+import burgerMenuIMG from "../../assets/shared/tablet/icon-hamburger.svg";
+import ThumbnailSection from "../thumbnail-section/ThumbnailSection";
 
 const customStyles = {
   overlay: {
     backgroundColor: "rgba(0, 0, 0, 0.50)",
     padding: "20px",
   },
-  content: {
-    top: "130px",
-    left: "unset",
-    right: "calc((100vw - 1110px)/2)",
-    width: "377px",
-    height: "fit-content",
-  },
 };
 
 const Navbar = () => {
   const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+  const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState<boolean>(false);
+
+  const toggleBurgerMenu = () => setIsBurgerMenuOpen(!isBurgerMenuOpen);
 
   return (
     <div className={styles.navbar}>
       <div className={`container ${styles.navbarContainer}`}>
-        <Link to={"/"}>
-          <img src={logo} alt="" />
-        </Link>
+        <div className={styles.navbarLeft}>
+          <button
+            onClick={toggleBurgerMenu}
+            className={styles.burgerMenuBtn}
+            type="button"
+          >
+            <img src={burgerMenuIMG} alt="" />
+          </button>
+          <Link className={styles.home} to={"/"}>
+            <img src={logo} alt="" />
+          </Link>
+        </div>
         <nav className={styles.links}>
-          <Link to={"/"}>Home</Link> 
+          <Link to={"/"}>Home</Link>
           <Link to={"/products/headphones"}>Headphones</Link>
           <Link to={"/products/speakers"}>Speakers</Link>
           <Link to={"/products/earphones"}>Earphones</Link>
@@ -40,11 +46,23 @@ const Navbar = () => {
           <IoCartOutline size={25} color="var(--color-text-two)" />
         </button>
       </div>
+      {isBurgerMenuOpen && (
+        <>
+          <button
+            className={styles.overlay}
+            onClick={toggleBurgerMenu}
+          ></button>
+          <div className={styles.burgerMenuContent}>
+            <ThumbnailSection />
+          </div>
+        </>
+      )}
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
         style={customStyles}
         ariaHideApp={false}
+        className={styles.modalContent}
       >
         <CartContent setIsOpen={setModalIsOpen} />
       </Modal>
